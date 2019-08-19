73bc3f7495032e94ea0a1ed25cee0164c7f55a45
PaulZChristy2017 <paul.christy2017@gmail.com>
2019-08-19 14:20:30 -0500

    Add files via upload

--- EventsPage.react.js
+++ EventsPage.react.js
@@ -74,28 +74,64 @@ function Events() {
     <SiteWrapper>
       <Page.Content>
         <MaterialTable
-          title="Refresh Data Preview"
+          title="Events"
           columns={[
             { title: 'Event Name', field: 'eventName' },
             { title: 'Venue Name', field: 'venueName' },
+            { title: 'Venue Address', field: 'venueAddress' },
+            { title: 'Event Date', field: 'eventDate' },
+            { title: 'Table Size', field: 'tableSize' },
+            { title: 'Expected Guests', field: 'expectedGuests' },
+            { title: '% of Empty Seats', field: 'emptyPercentage' },
+
           ]}
           data={query =>
             new Promise((resolve, reject) => {
               let url = 'https://eagleeventplanningapi.azurewebsites.net/ep/events'
-              //url += 'per_page=' + query.pageSize
-              //url += '&page=' + (query.page + 1)
               fetch(url)
                 .then(response => response.json())
                 .then(result => {
                   resolve({
                     data: result,
-                    //page: result.page - 1,
-                    //totalCount: result.total,
                   })
                 })
             })
           }
+
+        /*
+        editable={
+          {
+            onRowAdd: newData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  const data = [...state.data];
+                  data.push(newData);
+                  setState({ ...state, data });
+                }, 600);
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  const data = [...state.data];
+                  data[data.indexOf(oldData)] = newData;
+                  setState({ ...state, data });
+                }, 600);
+              }),
+            onRowDelete: oldData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  const data = [...state.data];
+                  data.splice(data.indexOf(oldData), 1);
+                  setState({ ...state, data });
+                }, 600);
+              }),
+          }
+        }*/
         />
+
 
         <button>
           Event Info
